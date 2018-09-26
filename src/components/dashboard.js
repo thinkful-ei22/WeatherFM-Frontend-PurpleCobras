c8a4339c7a56ec4762cb7a732331469530b2ba07
Brandon Graham <bgraham452@gmail.com>
2018-09-26 16:15:02 -0400

    Finally got the lat and long to dispatch to fetchWeather

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -6,37 +6,60 @@ import {fetchWeather} from '../actions/weather';
 
 
 export class Dashboard extends React.Component {
-    // api/users/weather
-    
-    componentDidMount() {
-        let location;
-        let latitude, longitude;
-        if ("geolocation" in navigator) {
-          /* geolocation is available */
-    
-          function geo_success(position) {
-            console.log(position.coords.latitude,',', position.coords.longitude);
-          }
-          
-          function geo_error() {
-            alert("Sorry, no position available.");
-          }
-    
-        location = navigator.geolocation.watchPosition(geo_success, geo_error);
-          
-        // console.log(latitude, longitude + '<<<outside of geo success');
+  constructor(props) {
+    super(props);
 
-          
-        } else {
-          /* geolocation IS NOT available */
-          console.log('geolocation is not available')
-        }
-          // this.props.dispatch(fetchWeather(lat, long));
-        
-      }
+    // this.state = {
+    //   latitude: 0,
+    //   longitude:0
+    // }
+  }    
+
+  getLocation = () => {
+    console.log('geoloc running');
+    let latitude, longitude;
+    console.log(this, '1st this');
+    const {dispatch} = this.props;
+    if ("geolocation" in navigator) {
+      /* geolocation is available */
+      navigator.geolocation.getCurrentPosition(function(position) {
+        console.log(position.coords.latitude, position.coords.longitude);
+
+        latitude=position.coords.latitude;
+        longitude=position.coords.longitude;
+        console.log(this, ' <THIS');
+        dispatch(fetchWeather(latitude, longitude));
+
+        console.log('at the end of function');
+      });
+
+
+    //   function geo_success(position) {
+    //     console.log(position.coords.latitude,',', position.coords.longitude);
+    //   }
       
+    //   function geo_error() {
+    //     alert("Sorry, no position available.");
+    //   }
+
+    // location = navigator.geolocation.watchPosition(geo_success, geo_error);
+      
+    // console.log(latitude, longitude + '<<<outside of geo success');
+
+    } else {
+      /* geolocation IS NOT available */
+      console.log('geolocation is not available')
+    }
+      // 
+     
+        //console.log('latitude received');
+      
+  }
+  
       
     render() {
+      this.getLocation();
+
         let weatherMessage;
         if (this.props.weather) {
           weatherMessage = <div>Right now it is {this.props.weather}!</div>
