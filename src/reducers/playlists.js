5a3d359d4e0a2e8606b4d67ec975ffbd4ae834b0
Kaitlin Bunn <kabunn1991@gmail.com>
2018-09-28 12:59:46 -0400

    delete on frontend functional

--- src/reducers/playlists.js
+++ src/reducers/playlists.js
@@ -1,11 +1,14 @@
 import {
   FETCH_PLAYLISTS_SUCCESS,
-  FETCH_PLAYLISTS_ERROR
+  FETCH_PLAYLISTS_ERROR,
+  DELETE_SONG_SUCCESS,
+  DELETE_SONG_ERROR
 } from '../actions/playlists';
 
 const initialState = {
   playlists: null,
-  error: null
+  error: null,
+  deleted: null
 }
 
 export default function reducer(state = initialState, action) {
@@ -13,11 +16,23 @@ export default function reducer(state = initialState, action) {
     console.log('FETCH PLAYLISTS SUCCESSFUL');
     return Object.assign({}, state, {
       playlists: action.playlists,
+      deleted: false,
       error: null
     })
   } else if (action.type === FETCH_PLAYLISTS_ERROR) {
     return Object.assign({}, state, {
       error: action.error
     })
-  } return state;
+  } else if (action.type === DELETE_SONG_SUCCESS) {
+    console.log('DELETE SONG SUCCESSFUL');
+    return Object.assign({}, state, {
+      deleted: true
+    })
+  } else if (action.type === DELETE_SONG_ERROR) {
+    console.log('DELETE SONG ERROR');
+    return Object.assign({}, state, {
+      error: action.error
+    })
+  }
+  return state;
 }
