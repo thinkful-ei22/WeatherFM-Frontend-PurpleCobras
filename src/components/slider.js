70b82fd268f86b4d37357c3a7f75e84d8d9b764c
Ian Beihl <ianbeihl@gmail.com>
2018-10-03 09:37:53 -0700

    added more adjectives and cleaned up code

--- src/components/slider.js
+++ src/components/slider.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import requiresLogin from './requires-login';
 import { fetchSpotifySlider } from '../actions/spotifySlider';
 import { fetchSpotify } from '../actions/spotify';
+import '../css/slider.css';
 
 export class Slider extends React.Component {
     constructor(props) {
@@ -18,7 +19,6 @@ export class Slider extends React.Component {
 
     handleSlider(e) {
         e.preventDefault();
-        console.log("NEW URLLLLL", this.props.url)
         let sliderObj = {
             danceability: e.target.danceability.value,
             energy: e.target.energy.value,
@@ -32,47 +32,65 @@ export class Slider extends React.Component {
     }
 
     resetSettings() {
-        console.log("RESET SETTINGS");
         this.props.dispatch(fetchSpotify(this.props.weather))
     }
-    //should add a switch statement with default settings for all these weathers....
-    //based on the average from our initial songs...
 
     toggleSliderDisplay() {
         this.setState({
             displaySlider: !this.state.displaySlider
         })
     }
+
     render() {
         let sliderForm = '';
         let sliderMessage = 'Show Advanced Settings'
         if (this.state.displaySlider === true) {
             sliderMessage = "Hide Advanced Settings"
             sliderForm =
-                <fieldset>
-                    <form onSubmit={(e) => this.handleSlider(e)}>
-                        <label htmlFor="danceability">Danceability</label>
-                        <input type="range" id="danceability" name="danceability"
-                            min="0" max="1" defaultValue=".5" step=".01" />
-                        <label htmlFor="energy">Energy</label>
-                        <input type="range" id="energy" name="energy"
-                            min="0" max="1" defaultValue=".5" step=".01" />
-                        <label htmlFor="popularity">Popularity</label>
-                        <input type="range" id="popularity" name="popularity"
-                            min="10" max="80" defaultValue="30" step="1" />
-                        <label htmlFor="valence">Valence</label>
-                        <input type="range" id="valence" name="valence"
-                            min="0" max="1" defaultValue=".5" step=".01" />
-                        <label htmlFor="loudness">Loudness</label>
-                        <input type="range" id="loudness" name="loudness"
-                            min="-60" max="0" defaultValue="-30" step=".5" />
-                        <label htmlFor="acousticness">acousticness</label>
-                        <input type="range" id="acousticness" name="acousticness"
-                            min="0" max="1" defaultValue=".5" step=".01" />
+                <div>
+                    <form onSubmit={(e) => console.log(e.target.value)} className="slider">
+                        <div className="dancy">
+                            <label htmlFor="danceability">Not Dancy</label>
+                            <input type="range" id="danceability" name="danceability"
+                                min="0" max="1" defaultValue=".5" step=".01"
+                                onChange={(e) => console.log(e.target.value)} />
+                            <label htmlFor="danceability">Super Dance</label>
+                        </div>
+                        <div className="energy">
+                            <label htmlFor="energy">Low Energy</label>
+                            <input type="range" id="energy" name="energy"
+                                min="0" max="1" defaultValue=".5" step=".01" />
+                            <label htmlFor="energy">High Energy</label>
+                        </div>
+                        <div className="popularity">
+                            <label htmlFor="popularity">Less Popular</label>
+                            <input type="range" id="popularity" name="popularity"
+                                min="10" max="80" defaultValue="30" step="1" />
+                            <label htmlFor="popularity">More Popular</label>
+                        </div>
+                        <div className="happiness">
+                            <label htmlFor="valence">Sad</label>
+                            <input type="range" id="valence" name="valence"
+                                min="0" max="1" defaultValue=".5" step=".01" />
+                            <label htmlFor="valence">Happy</label>
+                        </div>
+                        <div className="volume">
+                            <label htmlFor="loudness">Softer</label>
+                            <input type="range" id="loudness" name="loudness"
+                                min="-60" max="0" defaultValue="-30" step=".5" />
+                            <label htmlFor="loudness">Louder</label>
+                        </div>
+                        <div className="acousticness">
+                            <label htmlFor="acousticness">Less Acoustic</label>
+                            <input type="range" id="acousticness" name="acousticness"
+                                min="0" max="1" defaultValue=".5" step=".01" />
+                            <label htmlFor="acousticness">More Acoustic</label>
+                        </div>
                         <button type="submit">Customize!</button>
                     </form>
                     <button onClick={() => this.resetSettings()}>Reset to Weather Settings</button>
-                </fieldset>
+                </div>
+
         }
         return (
             <div className="slider">
@@ -83,15 +101,9 @@ export class Slider extends React.Component {
     }
 }
 
-const mapStateToProps = state => {
-    const { currentUser } = state.auth;
-    return {
-        username: state.auth.currentUser.username,
-        name: `${currentUser.firstName}`,
-        protectedData: state.protectedData.data,
+const mapStateToProps = state => ({
         weather: state.weather.weather,
         url: state.youtube.videoURL
-    };
-};
+});
 
 export default requiresLogin()(connect(mapStateToProps)(Slider));
