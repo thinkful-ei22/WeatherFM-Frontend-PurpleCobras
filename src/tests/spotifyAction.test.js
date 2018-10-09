3039c937b07c7311e0e03e94b42b553fe9ec7584
Kevin <ketsang12@gmail.com>
2018-10-09 15:06:27 -0400

    more tests added. all tests passing

--- src/tests/spotifyAction.test.js
+++ src/tests/spotifyAction.test.js
@@ -15,6 +15,19 @@ import {
   SYNC_SPOTIFY_SUCCESS,
   SYNC_SPOTIFY_ERROR} from '../actions/spotify';
 
+import { 
+  fetchSpotifySliderRequest,
+  fetchSpotifyAveragesSuccess,
+  updateSpotifyAverages,
+  fetchSpotifySliderError,
+  fetchSpotifySliderSuccess,
+  FETCH_SPOTIFY_SLIDER_REQUEST, 
+  FETCH_SPOTIFY_SLIDER_SUCCESS, 
+  FETCH_SPOTIFY_SLIDER_ERROR,
+  FETCH_SPOTIFY_AVERAGES_SUCCESS,
+  UPDATE_SPOTIFY_AVERAGES,
+} from '../actions/spotifySlider';
+
 describe('Spotify Actions', () => {
   it('Should make a spotify request action.', () => {
     const action = fetchSpotifyRequest();
@@ -48,5 +61,34 @@ describe('Spotify Actions', () => {
     const action = syncSpotifyError(obj);
     expect(action.type).toEqual(SYNC_SPOTIFY_ERROR);
     expect(action.error).toEqual(obj);
+  });
+
+  it('Should make a spotify slider request action.', () => {
+    const action = fetchSpotifySliderRequest();
+    expect(action.type).toEqual(FETCH_SPOTIFY_SLIDER_REQUEST);
+  });
+  it('Should return the songs when fetch spotify slider comes back successfully.', () => {
+    const obj = {song: 'test'};
+    const action = fetchSpotifySliderSuccess(obj);
+    expect(action.type).toEqual(FETCH_SPOTIFY_SLIDER_SUCCESS);
+    expect(action.songs).toEqual(obj);
+  });
+  it('Should return an error when fetch spotify slider comes back with an error.', () => {
+    const obj = {error: 400};
+    const action = fetchSpotifySliderError(obj);
+    expect(action.type).toEqual(FETCH_SPOTIFY_SLIDER_ERROR);
+    expect(action.error).toEqual(obj);
+  });
+  it('Should return the averages when fetch spotify averages come back successfully.', () => {
+    const obj = {avg: 'test'};
+    const action = fetchSpotifyAveragesSuccess(obj);
+    expect(action.type).toEqual(FETCH_SPOTIFY_AVERAGES_SUCCESS);
+    expect(action.averages).toEqual(obj);
+  });
+  it('Should return the songs when updated spotify average comes back successfully.', () => {
+    const obj = {avg: 'test'};
+    const action = updateSpotifyAverages(obj);
+    expect(action.type).toEqual(UPDATE_SPOTIFY_AVERAGES);
+    expect(action.average).toEqual(obj);
   });
 });
