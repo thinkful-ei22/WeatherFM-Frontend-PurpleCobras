b9e87a13e0cd7f4a6e046854d8be61583ce13a45
Kevin <ketsang12@gmail.com>
2018-10-09 14:53:18 -0400

    added more tests. all passing

--- src/tests/spotifyReducer.test.js
+++ src/tests/spotifyReducer.test.js
@@ -1,6 +1,7 @@
 /* global $ expect jest */
 import spotifyReducer from '../reducers/spotify';
-import {fetchSpotifySuccess} from '../actions/spotify';
+import {fetchSpotifySuccess, fetchSpotifyError} from '../actions/spotify';
+import {fetchSpotifySliderSuccess, fetchSpotifySliderError, fetchSpotifyAveragesSuccess, updateSpotifyAverages} from '../actions/spotifySlider';
 
 describe ('Spotify Reducers', () => {
   it('Should set the initial state when nothing is passed in.', () => {
@@ -20,5 +21,58 @@ describe ('Spotify Reducers', () => {
     const songs = {song: 'test'};
     const state = spotifyReducer(oldState, fetchSpotifySuccess(songs));
     expect(state.songs).toEqual(songs);
+  });
+  it('Should handle the fetch spotify error action.', () => {
+    const oldState = {
+      songs: [],
+      error: null,
+      averages: null
+    };
+    const error = '400';
+    const state = spotifyReducer(oldState, fetchSpotifyError(error));
+    console.log(state.error);
+    expect(state.error).toEqual(error);
+  });
+
+  it('Should handle the fetch spotify slider success action.', () => {
+    const oldState = {
+      songs: [],
+      error: null,
+      averages: null
+    };
+    const songs = 'test';
+    const state = spotifyReducer(oldState, fetchSpotifySliderSuccess(songs));
+    expect(state.songs).toEqual(songs);
+  });
+
+  it('Should handle the fetch spotify slider error action.', () => {
+    const oldState = {
+      songs: [],
+      error: null,
+      averages: null
+    };
+    const error = '400';
+    const state = spotifyReducer(oldState, fetchSpotifySliderError(error));
+    expect(state.error).toEqual(error);
+  });
+  it('Should handle the fetch spotify average success action.', () => {
+    const oldState = {
+      songs: [],
+      error: null,
+      averages: null
+    };
+    const averages = {avg1: 4};
+    const state = spotifyReducer(oldState, fetchSpotifyAveragesSuccess(averages));
+    expect(state.averages).toEqual(averages);
+  });
+  it('Should handle the fetch spotify slider success action.', () => {
+    const oldState = {
+      songs: [],
+      error: null,
+      averages: null
+    };
+    const averages = {avg1: 4};
+    const state = spotifyReducer(oldState, updateSpotifyAverages(averages));
+    expect(state.averages).toEqual(averages);
   });
 });
--- src/tests/weatherReducer.test.js
+++ src/tests/weatherReducer.test.js
@@ -1,8 +1,8 @@
 /* global $ expect jest */
 import weatherReducer from '../reducers/weather';
-import {fetchWeatherSuccess} from '../actions/weather';
+import {fetchWeatherSuccess, fetchWeatherError, setWeather, changeWeatherSuccess, changeWeatherError} from '../actions/weather';
 
-describe ('Spotify Reducers', () => {
+describe ('Weather Reducers', () => {
   it('Should set the initial state when nothing is passed in.', () => {
     const state = weatherReducer(undefined, {type: '@@UNKNOWN'});
     expect(state).toEqual({
@@ -27,5 +27,53 @@ describe ('Spotify Reducers', () => {
     expect(state.weather).toEqual(str1);
     expect(state.tempC).toEqual(str2);
     expect(state.tempF).toEqual(str3);
+  });
+  it('Should handle the fetch weather error action.', () => {
+    const oldState = {
+      weather: '',
+      error: null,
+      tempC: null,
+      tempF: null,
+    };
+    const str = '1';
+    const state = weatherReducer(oldState, 
+      fetchWeatherError(str));
+    expect(state.error).toEqual(str);
+  });
+  it('Should handle the set weather success action.', () => {
+    const oldState = {
+      weather: '',
+      error: null,
+      tempC: null,
+      tempF: null,
+    };
+    const str = 'test';
+    const state = weatherReducer(oldState, 
+      setWeather(str));
+    expect(state.weather).toEqual(str);
+  });
+  it('Should handle the change weather success action.', () => {
+    const oldState = {
+      weather: '',
+      error: null,
+      tempC: null,
+      tempF: null,
+    };
+    const str = 'test';
+    const state = weatherReducer(oldState, 
+      changeWeatherSuccess(str));
+    expect(state.weather).toEqual(str);
+  });
+  it('Should handle the change weather error action.', () => {
+    const oldState = {
+      weather: '',
+      error: null,
+      tempC: null,
+      tempF: null,
+    };
+    const str = 'test';
+    const state = weatherReducer(oldState, 
+      changeWeatherError(str));
+    expect(state.error).toEqual(str);
   });
 });
