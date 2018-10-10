b31aecfdeb1ff49472439b2495e7b90d2c0e87c4
Kaitlin Bunn <kabunn1991@gmail.com>
2018-10-10 12:00:24 -0400

    fixed form warning message

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -15,7 +15,7 @@ export class LoginForm extends React.Component {
     let error;
     if (this.props.error) {
       error = (
-        <div className="form-error" aria-live="polite">
+        <div className="form-error2" aria-live="polite">
           {this.props.error}
         </div>
       );
