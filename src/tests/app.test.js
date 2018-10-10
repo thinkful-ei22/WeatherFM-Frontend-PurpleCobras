1c0fd1c7fee12b37f3d1c647e66930d846c48d9c
Kevin <ketsang12@gmail.com>
2018-10-10 13:48:57 -0400

    .then travis ci fix

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -4,7 +4,7 @@ import {App} from '../components/app';
 
 describe('<App/>', () => {
   it('should render without crashing', () => {
-    global.fetch = jest.fn();
+    global.fetch = jest.fn().mockImplementation(() => Promise.resolve({ok: true}));;
     const wrapper = shallow(
     <App />);
   });
